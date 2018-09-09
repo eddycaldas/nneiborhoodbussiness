fcb894ad0294435778d27f2af5d68b302c3d8ffe
Eddy Caldas <eddycaldas@live.com>
2018-09-08 21:36:20 -0600

    add modal

--- public/app.js
+++ public/app.js
@@ -16,63 +16,76 @@ $(document).ready(function() {
           const isnum = zipCodeProvided.match(/^\d+$/); 
                     if(isnum && zipCodeProvided.length<6) {
                         for(var i=1; i<data.length; i++) {
-                          var showBusiness = data[i].zipcode
-                          var showName = data[i].name
-                          var showNumber = data[i].number
-                          var showAddress = data[i].address
-                          var showImage = data[i].image
-                          var showWebsite = data[i].website
-                          var showCategory = data[i].category
-                          var showHours = data[i].hours
-                          var showSummary = data[i].summary
-                          var showId = data[i].id
+                          
+                            var showBusiness = data[i].zipcode
+                            var showName = data[i].name
+                            var showNumber = data[i].number
+                            var showAddress = data[i].address
+                            var showImage = data[i].image
+                            var showWebsite = data[i].website
+                            var showCategory = data[i].category
+                            var showHours = data[i].hours
+                            var showSummary = data[i].summary
+                            var showId = data[i].id
+                            
+                              var array = []
+                            
+                                if( zipCodeProvided == showBusiness ) { 
+                                  array.push(showImage, showName, showNumber,
+                                              showAddress, showHours, showCategory,
+                                              showSummary, showWebsite);
+                                              // console.log(array);
 
-                                      if( zipCodeProvided == showBusiness ) { 
-                                      
-                                        $("#header").append( 
-                                                    `<div id='theDiv'>
-                                                    <img class="picture" src=${showImage} alt="picture"/>
-                                                  <h1 id='name'>${showName}</h1>
-                                                  <h2 id='address'>${showAddress}</h2>
-                                                  <h2 id='number'>${showNumber}</h2>
-                                                  </div>`
-                                                )
-                                        
-                        }    
+                                  
+                                      $("#header").append(     
+                                        `<div class='theDiv' id='theDiv'>
+                                            <img class="picture" src=${showImage} alt="picture"/>
+                                            <h1 id='name'>${showName}</h1>
+                                            <h2 id='address'>${showAddress}</h2>
+                                            <h2 id='number'>${showNumber}</h2>
+                                          </div>`
+                                            )
+                                          }  
+                                          
+                                          
+                        
+                    //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+                    $("div#theDiv.theDiv").on("click", function(){
+                      $(".popup-overlay, .popup-content").addClass("active");
+                    });
+
+                    //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+                    $(".close, .popup-overlay").on("click", function(){
+                      $(".popup-overlay, .popup-content").removeClass("active");
+                    });
+                                          
+        
+        
+        
+        
+                    var source   = $("#entry-template").html();
+                    var template = Handlebars.compile(source);            
+                    var context = {
+                      entry: array
+                    };
+                    var html    = template(context);
+                    console.log(array);
+                    $('.content').append(html);
+                    
+                    
                     
                         
-      }
-      
-                    } else {           
-                          $('#header2').append('Type a 5 digit zip code').css('color','white');
-                    }    
-                                 
-        });                         
+                                          
+                                        }
+                                        
+                                        
+                                        
+                                          
+                                      } else {           
+                      $('#header2').append('Type a 5 digit zip code').css('color','white');
+                    }                              
+        });                
     });
 });
-
-
-  
-                                                    // $("li").click( function() {
-                                                    //       $("li").removeClass('selected');
-                                                    //       $(this).addClass('selected');
-                                                    //     })
-
-
-
-
-
-      // $(document).ready(function() {
-      //   setTimeout(function(){
-      //     $("#dialogMsg").modal()
-      //   }, 3000)
-      // })
-      // 
-
-
-
-   
-      
-
 
 
