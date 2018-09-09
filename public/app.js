78edb6aa0ef0ea10cf86f78ef4fe00a766fb431e
Eddy Caldas <eddycaldas@live.com>
2018-09-09 09:40:44 -0600

    modal gets info now

--- public/app.js
+++ public/app.js
@@ -28,14 +28,8 @@ $(document).ready(function() {
                             var showSummary = data[i].summary
                             var showId = data[i].id
                             
-                              var array = []
-                            
                                 if( zipCodeProvided == showBusiness ) { 
-                                  array.push(showImage, showName, showNumber,
-                                              showAddress, showHours, showCategory,
-                                              showSummary, showWebsite);
-                                              // console.log(array);
-
+                              
                                   
                                       $("#header").append(     
                                         `<div class='theDiv' id='theDiv'>
@@ -43,38 +37,28 @@ $(document).ready(function() {
                                             <h1 id='name'>${showName}</h1>
                                             <h2 id='address'>${showAddress}</h2>
                                             <h2 id='number'>${showNumber}</h2>
+                                            
+                                            
+                                            <!-- Modal data HTML embedded directly into document -->
+                                                              <div id="ex1" class="modal">
+                                                                <p>Thanks for clicking. That felt good.</p>
+                                                                <img class="picture" src=${showImage} alt="picture"/>
+                                                                <h1 id='name'>${showName}</h1>
+                                                                <h2 id='address'>${showAddress}</h2>
+                                                                <h2 id='number'>${showNumber}</h2>
+                                                                <h2 id='showHours'>${showHours}</h2>
+            <!-- Don't change anything -->
+                                                                <a href="#" rel="modal:close">Close</a>
+                                                              </div>
+            <!-- Link to open the modal -->
+                                                              <p><a class='modal-info'href="#ex1" rel="modal:open"> ...more info</a>
+                                                              </p>            
+                                            
+                                            
                                           </div>`
                                             )
                                           }  
-                                          
-                                          
-                        
-                    //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
-                    $("div#theDiv.theDiv").on("click", function(){
-                      $(".popup-overlay, .popup-content").addClass("active");
-                    });
-
-                    //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-                    $(".close, .popup-overlay").on("click", function(){
-                      $(".popup-overlay, .popup-content").removeClass("active");
-                    });
-                                          
         
-        
-        
-        
-                    var source   = $("#entry-template").html();
-                    var template = Handlebars.compile(source);            
-                    var context = {
-                      entry: array
-                    };
-                    var html    = template(context);
-                    console.log(array);
-                    $('.content').append(html);
-                    
-                    
-                    
-                        
                                           
                                         }
                                         
